60f739e17ce67a95de17a1e0bac6966c1775c276
madfox <antoine.renard@gmail.com>
2022-10-19 13:57:21 +0200

    removed dist directory

--- dist/esm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CapacitorVolumeButtonsPlugin } from './definitions';
-declare const CapacitorVolumeButtons: CapacitorVolumeButtonsPlugin;
-export * from './definitions';
-export { CapacitorVolumeButtons };
--- dist/esm/index.js
+++
@@ -1,7 +0,0 @@
-import { registerPlugin } from '@capacitor/core';
-const CapacitorVolumeButtons = registerPlugin('CapacitorVolumeButtons', {
-    web: () => import('./web').then(m => new m.CapacitorVolumeButtonsWeb()),
-});
-export * from './definitions';
-export { CapacitorVolumeButtons };
-//# sourceMappingURL=index.js.map
